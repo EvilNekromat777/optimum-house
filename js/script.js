31694f17cd7bc35d57dbfc5b93e3bb6c75c09d58
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-02-24 22:41:51 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, World');
+//$(document).ready(function(){
+//	$('.slider').slick({
+//		arrows:true,
+//		dots:true,
+//		slidesToShow:1,
+//		autoplay:false,
+//		speed:1000,
+//		autoplaySpeed:800,
+//		responsive:[
+//			{
+//				breakpoint: 768,
+//				settings: {
+//					slidesToShow:2
+//				}
+//			},
+//			{
+//				breakpoint: 550,
+//				settings: {
+//					slidesToShow:1
+//				}
+//			}
+//		]
+//	});
+//});
