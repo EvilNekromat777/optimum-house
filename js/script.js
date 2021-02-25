757c5769c9d13023ecc5cd8ef2d4cd554f3296c8
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-02-25 10:03:07 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 //$(document).ready(function(){
 //	$('.slider').slick({
-//		arrows:true,
-//		dots:true,
-//		slidesToShow:1,
-//		autoplay:false,
-//		speed:1000,
-//		autoplaySpeed:800,
-//		responsive:[
-//			{
-//				breakpoint: 768,
-//				settings: {
-//					slidesToShow:2
-//				}
-//			},
-//			{
-//				breakpoint: 550,
-//				settings: {
-//					slidesToShow:1
-//				}
-//			}
-//		]
+//		//arrows:true,
+//		//dots:true,
+//		//slidesToShow:1,
+//		//autoplay:false,
+//		//speed:1000,
+//		//autoplaySpeed:800,
+//		//responsive:[
+//		//	{
+//		//		breakpoint: 768,
+//		//		settings: {
+//		//			slidesToShow:2
+//		//		}
+//		//	},
+//		//	{
+//		//		breakpoint: 550,
+//		//		settings: {
+//		//			slidesToShow:1
+//		//		}
+//		//	}
+//		//]
 //	});
 //});
+
+$('.slider').slick({
+	dots: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true
+ });
