f538ac7c86eba02a04403324e9ac576a94a3709b
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-02-26 14:19:06 +0300

    footer

--- js/script.js
+++ js/script.js
@@ -6,22 +6,28 @@ $('.slider').slick({
 	slidesToShow: 1,
 	adaptiveHeight: true
  });
- $('.slider2').slick({
-	dots: true,
-	infinite: true,
-	speed: 300,
-	slidesToShow: 1,
-	adaptiveHeight: true
- });
+// $('.slider2').slick({
+//	dots: true,
+//	infinite: true,
+//	speed: 300,
+//	slidesToShow: 1,
+//	adaptiveHeight: true
+// });
 // =============================================>
 //https://jqueryui.com/tabs/
-
-//$(document).ready(function(){
-//	$(".layout__button").click(function(){
-//		 $(this).toggleClass("active");
-//	});
-//});
 
 $( function() {
 	$( "#selectable" ).selectable();
  } );
+
+// =============================================>
+ //Скрыть placeholder при клике
+$(document).ready(function () {
+	$('input').focus(function () {
+		$(this).data('placeholder', $(this).attr('placeholder'))
+		$(this).attr('placeholder', '');
+	});
+	$('input').blur(function () {
+		$(this).attr('placeholder', $(this).data('placeholder'));
+	});
+});
