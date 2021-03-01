f8466ac2a38153e412c480353e3e59e4b1b721a3
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-03-01 11:30:15 +0300

    anim

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+//Прокрутка в верх страницы при перезагрузке
+$(document).ready(function(){
+	$(this).scrollTop(0);
+});
+
 
 $('.slider').slick({
 	dots: true,
@@ -6,13 +11,6 @@ $('.slider').slick({
 	slidesToShow: 1,
 	adaptiveHeight: true
  });
-// $('.slider2').slick({
-//	dots: true,
-//	infinite: true,
-//	speed: 300,
-//	slidesToShow: 1,
-//	adaptiveHeight: true
-// });
 // =============================================>
 //https://jqueryui.com/tabs/
 
@@ -180,8 +178,51 @@ function setCursorPosition(pos, e) {
 	setCursorPosition(3, input);
  });
 
-
+//Очищение формы при клике по сабмиту
  $('.button__mainform').click(function(){
 	$('.form')[0].reset();
 });
  
+//=========================================================================>
+
+
+
+
+//Анимация при скролле
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('active');
+			} else {
+				if (!animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.remove('active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
